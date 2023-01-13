562b444f5e7f784e0830eaf2a61d754501671747
ElliotRipa <elliot.ripa@gmail.com>
2023-01-13 09:54:22 +0000

    Fixed bug where shows could not be added.

--- html/api/developer-tools/addshow.php
+++ html/api/developer-tools/addshow.php
@@ -47,8 +47,8 @@ try {
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $sql = "INSERT INTO shows (name, start_date".$specifiedFields.", date_checked)
-  VALUES ($name, $start_date".$specifiedValues.", date("Y-m-d"))";
-  // use exec() because no results are returned
+  VALUES ($name, $start_date".$specifiedValues.", '".date("Y-m-d")."')";
+  //use exec() because no results are returned
   $conn->exec($sql);
   echo "New record created successfully";
 } catch(PDOException $e) {
