7ed2c2a60fb1e3475f27e7fdd10163feaf6d1550
ElliotRipa <elliot.ripa@gmail.com>
2023-02-21 12:06:34 +0000

    Added a developer tool for removing a movie.

---
+++ html/api/developer-tools/deletemovie.php
@@ -0,0 +1,45 @@
+<html>
+
+<form action="deletemovie.php" method="get">
+	ID: <input type="text" name="id"><br>
+<input type="submit">
+</form>
+
+</html>
+
+<?php
+
+$servername = "localhost";
+$username = "website";
+$password = "ne!JB9C2SK35";
+$dbname = "media";
+
+
+$id = $_GET["id"];
+
+if($id){
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "DELETE FROM movies
+  WHERE id = $id";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "Deleted movie number " . $id;
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+}
+
+function stringify($word) {
+
+	$output = "'".$word."'";
+
+	return $output;
+}
+
+?>
