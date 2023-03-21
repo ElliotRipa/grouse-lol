195797878385bbb3201a5db7100a7f443bc3ed60
ElliotRipa <elliot.ripa@gmail.com>
2023-03-21 22:26:54 +0000

    Fixed bug where apostrophes couldn't be used in show names.

--- html/api/developer-tools/primitive-add-episodes-from-csv.php
+++ html/api/developer-tools/primitive-add-episodes-from-csv.php
@@ -2,10 +2,10 @@
 
 $servername = "localhost";
 $username = "website";
-$password = "";
+$password = "insert password here";
 $dbname = "media2";
 
-$file = fopen("", "r");
+$file = fopen("insert file here", "r");
 
 $errorList = [];
 
@@ -39,11 +39,12 @@ if($file !== FALSE) {
 			endswitch;
 
 			$name = str_replace("'", "''", $data[0]);
+			$show = str_replace("'", "''", $data[6]);
 
 			if($source) {
-				$sql = "INSERT INTO movies (name, release_date, season, episode, imdb_rating, imdb_id, show, source) VALUES ('". $name . "', '" . $data[1] . "', '" . $data[2] . "', '" . $data[3] . "', '" . $data[4] . "', '" . $data[5] . "', '" . $data[6] . "', '" . $source . "')";
+				$sql = "INSERT INTO EpisodesExport (name, release_date, season, episode, imdb_rating, imdb_id, `show`, source) VALUES ('". $name . "', '" . $data[1] . "', '" . $data[2] . "', '" . $data[3] . "', '" . $data[4] . "', '" . $data[5] . "', '" . $show . "', '" . $source . "')";
 			} else {
-				$sql = "INSERT INTO movies (name, release_date, season, episode, imdb_rating, imdb_id, show) VALUES ('". $name . "', '" . $data[1] . "', '" . $data[2] . "', '" . $data[3] . "', '" . $data[4] . "', '" . $data[5] . "', '" . $data[6] . "')";
+				$sql = "INSERT INTO EpisodesExport (name, release_date, season, episode, imdb_rating, imdb_id, `show`) VALUES ('". $name . "', '" . $data[1] . "', '" . $data[2] . "', '" . $data[3] . "', '" . $data[4] . "', '" . $data[5] . "', '" . $show . "')";
 			}
 
 			try {
